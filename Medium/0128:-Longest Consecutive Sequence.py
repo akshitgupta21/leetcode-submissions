95ec40a414a8e71d79f2d9d26b444174ac30c3e0
akshitgupta21 <akshitgupta1610@gmail.com>
2026-08-06 15:32:06 +0530

    Create 0128:-Longest Consecutive Sequence.py

---
+++ Medium/0128:-Longest Consecutive Sequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        s=list(set(nums))
+        s.sort()
+        count=0
+        last_smaller=float("-inf")
+        longest=0
+        for i in range(len(s)):
+            if last_smaller==s[i]-1:
+                last_smaller=s[i]
+                count+=1
+            elif s[i]-1!=last_smaller:
+                count=1
+                last_smaller=s[i]
+            longest=max(longest,count)
+        return longest
